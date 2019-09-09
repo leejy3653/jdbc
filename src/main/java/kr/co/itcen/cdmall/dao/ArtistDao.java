3bf785ac039a7363efd028dded8b6abf52acd58b
ljy <leejy3653@naver.com>
2019-09-09 15:23:05 +0900

    ArtistDao Test end

--- src/main/java/kr/co/itcen/cdmall/dao/ArtistDao.java
+++ src/main/java/kr/co/itcen/cdmall/dao/ArtistDao.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,24 +16,37 @@ public class ArtistDao {
 	public Boolean insert(ArtistVo vo1) {
 		Boolean result = false;
 		Connection connection = null;
+		Statement stmt = null;
 		PreparedStatement pstmt = null;
+		ResultSet rs = null;
 
 		try {
 			connection = getConnection();
 
 			String sql = "insert into artist values(null, ?)";
 			pstmt = connection.prepareStatement(sql);
-
 			pstmt.setString(1, vo1.getName());
-
 			int count = pstmt.executeUpdate();
-
 			result = (count == 1);
+			
+			stmt = connection.createStatement();
+			rs = stmt.executeQuery("select last_insert_id()");
+			if (rs.next()) {
+				Long no = rs.getLong(1);
+				vo1.setNo(no);
+			}
 
 		} catch (SQLException e) {
 			System.out.println("error:" + e);
 		} finally {
 			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (stmt != null) {
+					stmt.close();
+				}
+
 				if (pstmt != null) {
 					pstmt.close();
 				}
@@ -107,5 +121,33 @@ public class ArtistDao {
 		}
 
 		return connection;
+	}
+
+	public void delete() {
+		Connection connection = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			connection = getConnection();
+
+			String sql = "delete from artist";
+			pstmt = connection.prepareStatement(sql);
+
+			pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (connection != null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
